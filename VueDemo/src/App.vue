26d71076b716f8f8d729fca70799a80a3f3b34bd
WangTing <email@1947475827@qq.com>
2018-11-11 17:51:32 +0800

    first commit

---
+++ VueDemo/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <TodoHeader :addTodo="addTodo"/>
+      <TodoMain :todos="todos" :deleteTodo="deleteTodo"/>
+      <TodoFooter/>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      todos: [
+        {title:'吃饭',complete:false},
+        {title:'睡觉',complete:true},
+        {title:'打豆豆',complete:false},
+      ]
+    }
+  },
+  methods:{
+      //更新数据的方法
+      addTodo(todo){
+          //将数据添加到数组的头部，this是vm
+          this.todos.unshift(todo);
+      },
+    deleteTodo(index){
+          this.todos.splice(index,1);
+    }
+  },
+  components: {
+    TodoHeader:Header,
+    TodoMain:Main,
+    TodoFooter:Footer,
+  }
+}
+</script>
+
+<style>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
---
+++ VueDemo/src/components/Item.vue
@@ -0,0 +1,87 @@
+<template>
+  <li :style="{background:bgColor}" @mouseenter="handleEnter(true)" @mouseleave="handleEnter(false)" >
+    <label>
+      <input type="checkbox" v-model="todo.complete"/>
+      <span>{{todo.title}}</span>
+    </label>
+    <button class="btn btn-danger" v-show="isShow" @click="deleteItem">删除</button>
+  </li>
+</template>
+
+<script>
+export default{
+    props:{
+        todo:Object,
+        deleteTodo:Function,
+        index:Number,
+    },
+    data(){
+        return{
+            //初始状态
+          bgColor:'write',
+          isShow:false,
+        }
+    },
+    methods:{
+      handleEnter(isEnter){
+          //更新状态----进入
+        /*
+         if(isEnter){
+         this.bgColor ='#ccc';
+         this.isShow = true;
+         }else{
+         //鼠标移出
+         this.bgColor = '#fff';
+         this.isShow = false;
+         }
+         */
+        this.bgColor = isEnter ? '#cccccc' : '#ffffff';
+        this.isShow = isEnter;
+      },
+      deleteItem(){
+        const {todo,deleteTodo,index} = this;
+        if(confirm(`确定删除${todo.title}}吗？`)){
+            deleteTodo(index)
+        }
+      }
+  }
+}
+</script>
+
+
+<style scoped>
+  li {
+    list-style: none;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 5px;
+    border-bottom: 1px solid #ddd;
+  }
+
+  li label {
+    float: left;
+    cursor: pointer;
+  }
+
+  li label li input {
+    vertical-align: middle;
+    margin-right: 6px;
+    position: relative;
+    top: -1px;
+  }
+
+  li button {
+    float: right;
+   /* display: none;*/
+    margin-top: 3px;
+  }
+
+  li:before {
+    content: initial;
+  }
+
+  li:last-child {
+    border-bottom: none;
+  }
+
+</style>
